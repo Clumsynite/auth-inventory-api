d263edc087743454c829dfb3ad8fbf5e28d56326
Rishabh Pathak <clusmsyknight@gmail.com>
2021-06-05 16:06:42 +0530

    removed code to upload images

--- index.js
+++ index.js
@@ -17,8 +17,12 @@ require("./config");
 
 app.use(helmet());
 app.use(logger("dev"));
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+/* Below lines to be uncommented if you want to uploaded images as base64
+  // app.use(express.json({ limit: "50mb" }));
+  // app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+*/
 
 // CORS ----
 app.use(
